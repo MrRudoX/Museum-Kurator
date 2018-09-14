9736ebb77768cdbcf32afdb01b362cc90bb26f4e
MrRudoX <mrrudox@gmail.com>
2018-09-15 00:08:35 +0200

    Modified CSS

---
+++ Test.ino
@@ -0,0 +1,34 @@
+#define FACTORYRESET_ENABLE      1
+
+    #define MANUFACTURER_APPLE         "0x004C"
+    #define MANUFACTURER_NORDIC        "0x0059"
+
+    #define BEACON_MANUFACTURER_ID     MANUFACTURER_APPLE
+    #define BEACON_UUID                "01-12-23-34-45-56-67-78-89-9A-AB-BC-CD-DE-EF-F0"
+    #define BEACON_MAJOR               "0x0001"
+    #define BEACON_MINOR               "0x0002"
+    #define BEACON_RSSI_1M             "-54"
+
+Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Setting beacon configuration details: "));
+
+  // AT+BLEBEACON=0x004C,01-12-23-34-45-56-67-78-89-9A-AB-BC-CD-DE-EF-F0,0x0000,0x0000,-54
+  ble.print("AT+BLEBEACON="        );
+  ble.print(BEACON_UUID            ); ble.print(',');
+  ble.print(BEACON_MAJOR           ); ble.print(',');
+  ble.print(BEACON_MINOR           ); ble.print(',');
+  ble.print(BEACON_RSSI_1M         );
+  ble.println(); // print line causes the command to execute
+
+  // check response status
+  if (! ble.waitForOK() ) {
+    error(F("Didn't get the OK"));
+  }
+
+  Serial.println();
+  Serial.println(F("Open your beacon app to test"));
+}
+}
